6988c01fa65a57ed0c7d9832bb5633a15b47f711
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-01-18 17:39:55 -0300

    add Profile

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,8 @@
+import { Profile } from './components/Profile'
 import { HomeContainer } from './styles'
 
 export const Home = () => (
   <HomeContainer>
-    <h1>Blog</h1>
+    <Profile />
   </HomeContainer>
 )
