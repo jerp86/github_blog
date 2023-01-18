4025f61bbe1acaf5209e8050ab283791f9eaef4d
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-01-18 20:37:01 -0300

    remove container

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,12 +1,5 @@
 import styled from 'styled-components'
 
-export const HomeContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-`
-
 export const PostsListContainer = styled.section`
   width: 100%;
   display: grid;
