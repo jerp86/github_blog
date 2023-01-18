84dd53a394d6be531ba89d6a6ce1ecf02f475317
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-01-18 00:16:21 -0300

    add pages in Router

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,8 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
 import { DefaultLayout } from './layouts/DefaultLayout'
+import { Home } from './pages/Home'
+import { Post } from './pages/Post'
 
 export const Router = () => (
   <Routes>
-    <Route path="/" element={<DefaultLayout />} />
+    <Route path="/" element={<DefaultLayout />}>
+      <Route path="/" element={<Home />} />
+      <Route path="/post/:id" element={<Post />} />
+    </Route>
   </Routes>
 )
