4e735e94a71534aebddf7baf746db8c9763091b3
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-01-18 20:36:31 -0300

    create PostsListContainer

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -6,3 +6,11 @@ export const HomeContainer = styled.div`
   align-items: center;
   justify-content: center;
 `
+
+export const PostsListContainer = styled.section`
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(41.6rem, 1fr));
+  gap: 3.2rem;
+  margin-bottom: 7.2rem;
+`
