4ec6bd3329d9bc13cec68704237290229c6439fc
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-01-17 23:30:45 -0300

    add lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { ThemeProvider } from "styled-components";
-import { GlobalStyles } from "./styles/global";
-import { defaultTheme } from "./styles/themes/default";
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyles } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 export const App = () => (
   <ThemeProvider theme={defaultTheme}>
