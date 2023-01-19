2d56b53900a29a7f95469ebdb6d72e1672b5dcd5
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-01-19 00:10:54 -0300

    refactor adjust icon

--- src/components/ExternalLink/index.tsx
+++ src/components/ExternalLink/index.tsx
@@ -1,24 +1,21 @@
-import { IconDefinition } from '@fortawesome/free-solid-svg-icons'
+import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { ComponentProps } from 'react'
+import { ComponentProps, ReactNode } from 'react'
 import { ExternalLinkContainer } from './styles'
 
 type ExternalLinkProps = ComponentProps<typeof ExternalLinkContainer> & {
-  icon: IconDefinition
-  variant?: 'left' | 'right'
+  icon?: ReactNode
+  variant?: 'iconLeft'
 }
 
 export const ExternalLink = ({
   children,
   icon,
-  variant = 'right',
   ...rest
 }: ExternalLinkProps) => (
   <ExternalLinkContainer {...rest}>
-    {variant === 'left' && <FontAwesomeIcon icon={icon} />}
-
     {children}
 
-    {variant === 'right' && <FontAwesomeIcon icon={icon} />}
+    {icon ?? <FontAwesomeIcon icon={faArrowUpRightFromSquare} />}
   </ExternalLinkContainer>
 )
