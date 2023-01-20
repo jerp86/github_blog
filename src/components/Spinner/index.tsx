8a5e1c6b9f83b7853427763f401152658e1a1e49
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-01-19 22:06:38 -0300

    create Spinner component

---
+++ src/components/Spinner/index.tsx
@@ -0,0 +1,14 @@
+import { SpinnerContainer } from './styles'
+
+export const Spinner = () => (
+  <SpinnerContainer>
+    <div>
+      <div className="sk-chase-dot"></div>
+      <div className="sk-chase-dot"></div>
+      <div className="sk-chase-dot"></div>
+      <div className="sk-chase-dot"></div>
+      <div className="sk-chase-dot"></div>
+      <div className="sk-chase-dot"></div>
+    </div>
+  </SpinnerContainer>
+)
