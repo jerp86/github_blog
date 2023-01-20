6e3b12c10301fdb554920bb5b5b1eceb24c461f5
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-01-19 23:48:32 -0300

    refactored by adding posts from context and props

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,18 +1,30 @@
+import { useContextSelector } from 'use-context-selector'
+import { Spinner } from '../../components'
+import { GithubContext } from '../../context/GithubContext'
 import { PostFromList, Profile, SearchInput } from './components'
 import { PostsListContainer } from './styles'
 
-export const Home = () => (
-  <>
-    <Profile />
-    <SearchInput />
+export const Home = () => {
+  const { isLoading, posts } = useContextSelector(
+    GithubContext,
+    ({ isLoading, posts }) => ({
+      isLoading,
+      posts,
+    }),
+  )
 
-    <PostsListContainer>
-      <PostFromList />
-      <PostFromList />
-      <PostFromList />
-      <PostFromList />
-      <PostFromList />
-      <PostFromList />
-    </PostsListContainer>
-  </>
-)
+  return (
+    <>
+      <Profile />
+      <SearchInput numberOfPosts={posts.length} />
+
+      <PostsListContainer>
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          posts.map((post) => <PostFromList key={post.number} post={post} />)
+        )}
+      </PostsListContainer>
+    </>
+  )
+}
