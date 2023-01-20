efbf25769cfd2ba73632e214533b31ec263f079c
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-01-20 12:35:42 -0300

    create PostContent

---
+++ src/pages/Post/components/PostContent/styles.ts
@@ -0,0 +1,40 @@
+import styled from 'styled-components'
+
+export const PostContentContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 1.6rem;
+  padding: 4rem 3.2rem;
+  margin-bottom: 12.8rem;
+
+  img {
+    width: 100%;
+  }
+
+  h1,
+  h2,
+  h3 {
+    color: ${({ theme }) => theme.blue};
+  }
+
+  ul {
+    list-style: inherit;
+    padding-left: 2.4rem;
+  }
+
+  pre {
+    background: ${({ theme }) => theme.post};
+    padding: 1.6rem;
+
+    > div {
+      background: none !important;
+      padding: 0 !important;
+      margin: 0 !important;
+
+      code {
+        font-family: 'Fira Code', monospace;
+        line-height: 160% !important;
+      }
+    }
+  }
+`
--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,7 +1,44 @@
+import { useCallback, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { Spinner } from '../../components'
+import { IPost } from '../../context/GithubContext'
+import { api } from '../../lib/api'
+import { PostContent } from './components/PostContent'
 import { PostHeader } from './components/PostHeader'
 
-export const Post = () => (
-  <>
-    <PostHeader />
-  </>
-)
+const username = import.meta.env.VITE_GITHUB_USERNAME
+const repo = import.meta.env.VITE_GITHUB_REPOSITORY
+
+export const Post = () => {
+  const [isLoading, setIsLoading] = useState(true)
+  const [post, setPost] = useState({} as IPost)
+
+  const { id = '' } = useParams()
+
+  const getPostData = useCallback(async () => {
+    try {
+      setIsLoading(true)
+
+      const response = await api.get(`/repos/${username}/${repo}/issues/${id}`)
+
+      setPost(response.data as IPost)
+    } finally {
+      setIsLoading(false)
+    }
+  }, [id])
+
+  useEffect(() => {
+    getPostData()
+  }, [getPostData])
+
+  if (isLoading) {
+    return <Spinner />
+  }
+
+  return (
+    <>
+      <PostHeader postData={post} />
+      <PostContent content={post.body} />
+    </>
+  )
+}
